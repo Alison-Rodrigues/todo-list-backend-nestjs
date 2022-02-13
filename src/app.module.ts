c2e1d42a15e151cc2f22abde2bfca7a2d00e968d
Alison Rodrigues <alison.si1302@gmail.com>
2022-02-13 10:12:12 -0300

    Created the todo module, controller and service

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './app/todo/todo.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: Number(configService.get('DB_PORT', 5432)),
+        username: configService.get('DB_USER', 'todoadmin'),
+        password: configService.get('DB_PASSWORD', 'todo123'),
+        database: configService.get('DB_DATABASE', 'todo-list'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TodoModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
